828f479ceafed06e4e1ceaca23a8781a61fac75e
malik cheikh <malikcheikhpro14@gmail.com>
2023-08-01 11:28:19 +0200

    commit

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,6 +10,16 @@ class MainController extends AbstractController
     #[Route('/', name: 'home')]
     public function home()
     {
-        return $this->render('home.html.twig'); // Remplacez 'home.html.twig' par le nom de votre template Twig pour la page d'accueil
+        #return $this->render('home.html.twig'); // Remplacez 'home.html.twig' par le nom de votre template Twig pour la page d'accueil
+        echo 'coucou';
+        die();
+    }
+
+    #[Route('/test', name: 'home_test')]
+    public function test()
+    {
+        #return $this->render('home.html.twig'); // Remplacez 'home.html.twig' par le nom de votre template Twig pour la page d'accueil
+        echo 'testounet';
+        die();
     }
 }
